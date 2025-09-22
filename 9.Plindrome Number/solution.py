959145cd4bbc5fc06c9b8c4d35bfbb1ee55d6488
Abu Bakkar Siddique Sharif <abubakkarsiddiquesharif@gmail.com>
2025-09-22 23:17:10 +0600

    solved(9): Palindrome Number

---
+++ 9.Plindrome Number/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+
+        original = x
+        numReversed = 0
+        while x > 0:
+            lastDigit = x % 10
+            numReversed = numReversed * 10 + lastDigit
+            x = x // 10
+        return  numReversed
